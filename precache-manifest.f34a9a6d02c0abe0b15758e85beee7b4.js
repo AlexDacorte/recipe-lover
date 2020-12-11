0952c9887e79801612deeaacf19d674658d183f9
Alexsandro <dacortejhexson@gmail.com>
2020-12-11 18:00:48 -0500

    Updates

---
+++ precache-manifest.f34a9a6d02c0abe0b15758e85beee7b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b72f1b6541192ca17d5ea7375c37cb",
+    "url": "/recipe-lover/index.html"
+  },
+  {
+    "revision": "73ef4ed5c6dc339362f3",
+    "url": "/recipe-lover/static/css/main.18a7a710.chunk.css"
+  },
+  {
+    "revision": "c75e9f3bf8eb44707beb",
+    "url": "/recipe-lover/static/js/2.fa7178cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipe-lover/static/js/2.fa7178cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73ef4ed5c6dc339362f3",
+    "url": "/recipe-lover/static/js/main.097eabd7.chunk.js"
+  },
+  {
+    "revision": "10bb155955c420688da7",
+    "url": "/recipe-lover/static/js/runtime-main.28e7ad5d.js"
+  }
+]);
